b06bd9b25360ee3fc8789a5d55804dc6e2457c5b
pkrishnavamsi0709 <82568486+pkrishnavamsi0709@users.noreply.github.com>
2024-05-23 14:29:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/chatbot')
+def chatbot():
+    return "Hi I am Ai bot How can I help you"
 
     
 
